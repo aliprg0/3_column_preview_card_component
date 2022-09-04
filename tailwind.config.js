f8ee118c85a3f78c300ba16e208b5abe9d62350a
aliprg0 <aliprg0@gmail.com>
2022-09-04 18:46:45 +0430

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "bright-orange": "hsl(31, 77%, 52%)",
+        "dark-cyan": "hsl(184, 100%, 22%)",
+        "very-dark-cyan": "hsl(179, 100%, 13%)",
+      },
+    },
+  },
+  plugins: [],
+};
